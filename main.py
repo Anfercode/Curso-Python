68b9c1682d8663838430d55139374312ef1763ae
Andres <andesecci12@gmail.com>
2019-06-20 17:00:45 -0500

    Se añande el primer commit del proyecto, Funciones en python

---
+++ main.py
@@ -0,0 +1,26 @@
+
+
+clients = 'tomas,juan,'
+
+def create_client(client_name):
+    global clients
+    
+    clients += client_name
+    _add_comma()
+
+def list_clients():
+    global clients
+
+    print(clients)
+
+
+def _add_comma():
+    global clients
+
+    clients += ','
+
+
+if __name__ == '__main__':
+    list_clients()
+    create_client('David')
+    list_clients()
