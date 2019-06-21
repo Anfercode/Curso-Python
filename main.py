8afea70ae863c18f7e33e691faaca656ad5c3d26
Andres <andesecci12@gmail.com>
2019-06-21 16:20:35 -0500

    Se adiciona el comando de actualizacion de los datos de los clientes

--- main.py
+++ main.py
@@ -15,30 +15,48 @@ def list_clients():
     global clients
 
     print(clients)
+    print(clients)
 
+def update_client(client_name,updated_client_name):
+    global clients
+    if client_name in clients:
+        clients = clients.replace(client_name + ',' ,updated_client_name +',')
+        list_clients()
+    else:
+        print('client is not in client list')
+    
 
 def _add_comma():
     global clients
 
     clients += ','
 
+def _get_client_name():
+    return input('What is the client name: ')
+
 def _print_welcome():
     print('WELCOME TO PLATZI VENTAS')
     print('*'*50)
     print('what do you do today?')
     print('[C]reate Client')
+    print('[U]pdate Client')
     print('[D]elete Client')
 
 if __name__ == '__main__':
     _print_welcome()
     
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name: ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_clients()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('what is the updated client name')
+        update_client(client_name , updated_client_name)
     else:
         print('Invalid Command')
