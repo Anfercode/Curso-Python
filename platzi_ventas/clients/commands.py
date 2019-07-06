f0bf2892c652b79998e72fab6b35ad308df7d325
“Andres <andesecci12@gmail.com>
2019-07-05 22:29:13 -0500

    Se adicionan los metodos de borrado, listado y actualizacion junto con sus comandos

--- platzi_ventas/clients/commands.py
+++ platzi_ventas/clients/commands.py
@@ -40,21 +40,65 @@ def create(ctx, name, company, email, position):
 @click.pass_context
 def list(ctx):
     '''List all clients'''
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
 
+    client_list = client_service.list_clients()
+
+    click.echo('  ID  |  NAME |  COMPANY |  EMAIL | POSITION')
+    click.echo('*'*100)
+
+    for client in client_list:
+        click.echo(f"{client['uid']} | {client['name']} | {client['company']} | {client['email']} | {client['position']}")
 
 @clients.command()
 @click.pass_context
+@click.argument('client_uid',type=str)
 def update(ctx, client_uid):
     '''Updates a clients'''
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = _update_client_flow(Client(**client[0]))
+        client_service.update_client(client)
+
+        click.echo('Client Updated')
+    else:
+        click.echo('Client not found')
+
+
+def _update_client_flow(client):
+    click.echo('Leave empty if you dont want to modify the value')
+
+    client.name  =click.prompt('New name', type = str, default=client.name)
+    client.company  =click.prompt('New company', type = str, default=client.company)
+    client.email  =click.prompt('New email', type = str, default=client.email)
+    client.position  =click.prompt('New position', type = str, default=client.position)
+
+    return client
 
 
 @clients.command()
 @click.pass_context
+@click.argument('client_uid',type=str)
 def delete(ctx, client_uid):
     '''Deletes a client'''
-    pass
+    client_service = ClientService(ctx.obj['clients_table'])
+
+    client_list = client_service.list_clients()
+
+    client = [client for client in client_list if client['uid'] == client_uid]
+
+    if client:
+        client = Client(**client[0])
+        client_service.delete_client(client)
+
+        click.echo('Client Deleted')
+    else:
+        click.echo('Client not found')
 
 
 all = clients
--- platzi_ventas/clients/services.py
+++ platzi_ventas/clients/services.py
@@ -1,14 +1,57 @@
 
 import csv
+import os
 
 from clients.models import Client
+
 
 class ClientService:
 
     def __init__(self, table_name):
         self.table_name = table_name
 
+
     def create_client(self, client):
         with open(self.table_name, mode='a') as f:
-            writer = csv.DictWriter(f, fieldnames= Client.schema())
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
             writer.writerow(client.to_dict())
+
+
+    def list_clients(self):
+        with open(self.table_name, mode='r') as f:
+            reader = csv.DictReader(f, Client.schema())
+
+            return list(reader)
+
+
+    def update_client(self, updated_client):
+        clients = self.list_clients()
+
+
+        updated_clients = []
+        for client in clients:
+            if client['uid'] == updated_client.uid:
+                updated_clients.append(updated_client.to_dict())
+            else:
+                updated_clients.append(client)
+
+        self._save_to_disk(updated_clients)
+
+
+    def delete_client(self, deleted_client):
+        clients = self.list_clients()
+
+        for index, client in enumerate(clients):
+            if client['uid'] == deleted_client.uid:
+                clients.pop(index)
+
+        self._save_to_disk(clients)
+
+    def _save_to_disk(self, clients):
+        tmp_table_name = self.table_name+'.tmp'
+        with open(tmp_table_name, 'w') as f:
+            writer = csv.DictWriter(f, Client.schema())
+            writer.writerows(clients)
+
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
